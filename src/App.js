58912f11e2f32c8d68adc8f95493747b130f1033
Andres Higuita <yecid3257@live.com>
2021-04-08 10:29:07 -0500

    Solve issue

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.repeater = this.repeater.bind(this);
+    this.state = {
+      write: '',
+    };
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input
+          type="text"
+          placeholder="Empieza a escribir algo"
+          onChange={this.repeater}
+        />
+        <p className="repeater">{this.state.write}</p>
       </div>
     );
   }
+  repeater(e) {
+    this.setState({ write: e.target.value });
+  }
 }
-
 export default App;
